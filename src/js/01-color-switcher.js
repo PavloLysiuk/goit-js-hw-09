8643ee759530edc9eb9e4f58b39334e4d3d60be1
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-08 18:29:55 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,16 @@
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
+const selectors = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body')
+}
+
+// selectors.startBtn.addEventListener('click', onClickStart);
+// selectors.stopBtn.setAttribute('disabled', '');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
