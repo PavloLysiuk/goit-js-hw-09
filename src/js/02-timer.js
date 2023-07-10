b4da0ca86faf07390e4e2eda64a0ad687f228ad6
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-10 19:01:15 +0300

    task 2 update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,20 +20,16 @@
 // Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
 // Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
 // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
-// Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
-// Відлік часу
-// Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
+//* Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+//* Відлік часу
+//* Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
 
-// Кількість днів може складатися з більше, ніж двох цифр.
-// Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
+//* Кількість днів може складатися з більше, ніж двох цифр.
+//* Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
 // НЕ БУДЕМО УСКЛАДНЮВАТИ
 // Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
 
 // Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 // Форматування часу
 // Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто, якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію addLeadingZero(value), яка використовує метод padStart() і перед рендерингом інтефрейсу форматує значення.
@@ -57,7 +53,7 @@ const selectors = {
   seconds: document.querySelector('span[data-seconds]'),
 };
 
-let timeMS = 0;
+let timerTime = 0;
 
 const options = {
   enableTime: true,
@@ -65,8 +61,8 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    timeMS = Date.parse(selectedDates[0]) - Date.parse(this.defaultDate);
-    dateInFuture(timeMS);
+    timerTime = Date.parse(selectedDates[0]) - Date.parse(options.defaultDate);
+    dateInFuture(timerTime);
   },
 };
 
@@ -76,9 +72,9 @@ selectors.startBtn.disabled = true;
 function dateInFuture(time) {
   if (time < 0) {
     Notify.failure('Please choose a date in the future', {
-      width: '350px',
-      position: 'left-top',
-      fontSize: '12px',
+      width: '380px',
+      position: 'center-top',
+      fontSize: '18px',
     });
   } else {
     enableStartBtn();
@@ -98,20 +94,22 @@ function enableStartBtn() {
 
 
 
+function addLeadingZero(value) {
+  const keys = Object.keys(value);
+  for (const key of keys) {
+    value[key] = String(value[key]).padStart(2, '0');
+  }
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
